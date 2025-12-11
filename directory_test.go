435c55de551f791c0a9f0a99abeb5678edb1bbd2
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-11 19:42:56 +0000

    reformatted

--- directory_test.go
+++ directory_test.go
@@ -9,13 +9,15 @@ import (
 
 func TestDirectory(t *testing.T) {
 	d := dir{}
-	_, err := d.Open()
-	if err != fs.ErrPermission {
+
+	if _, err := d.Open(); err != fs.ErrPermission {
 		t.Errorf("expecting permission error, got: %s", err)
 	}
+
 	if d.Mode() != ModeDir {
 		t.Errorf("expecting mode %v, got %v", ModeDir, d.Mode())
 	}
+
 	mt := time.Now()
 	d = dir{
 		index: true,
@@ -25,66 +27,86 @@ func TestDirectory(t *testing.T) {
 		},
 		modTime: mt,
 	}
+
 	if !d.ModTime().Equal(mt) {
 		t.Errorf("expecting modTime %v, got %v", mt, d.ModTime())
 	}
+
 	f, err := d.Open()
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
+
 		return
 	}
+
 	c, err := f.Readdir(-1)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
+
 		return
 	}
+
 	if len(c) != 2 {
 		t.Errorf("expecting 2 items, got %d", len(c))
+
 		return
 	}
+
 	if c[0].Name() != "file1" {
 		t.Errorf("expecting file with name \"file1\", got %q", c[0].Name())
 	}
+
 	if c[1].Name() != "file2" {
 		t.Errorf("expecting file with name \"file2\", got %q", c[1].Name())
 	}
-	_, err = f.Readdir(1)
-	if err != io.EOF {
+
+	if _, err = f.Readdir(1); err != io.EOF {
 		t.Errorf("expected error EOF, got %s", err)
+
 		return
 	}
-	_, err = f.Seek(0, 0)
-	if err != nil {
+
+	if _, err = f.Seek(0, 0); err != nil {
 		t.Errorf("unexpected error: %s", err)
+
 		return
 	}
+
 	c, err = f.Readdir(1)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
+
 		return
 	}
+
 	if len(c) != 1 {
 		t.Errorf("expecting 1 item, got %d", len(c))
+
 		return
 	}
+
 	if c[0].Name() != "file1" {
 		t.Errorf("expecting file with name \"file1\", got %q", c[0].Name())
 	}
+
 	c, err = f.Readdir(1)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
+
 		return
 	}
+
 	if len(c) != 1 {
 		t.Errorf("expecting 1 item, got %d", len(c))
+
 		return
 	}
+
 	if c[0].Name() != "file2" {
 		t.Errorf("expecting file with name \"file2\", got %q", c[0].Name())
 	}
-	_, err = f.Readdir(1)
-	if err != io.EOF {
+
+	if _, err = f.Readdir(1); err != io.EOF {
 		t.Errorf("expected error EOF, got %s", err)
-		return
 	}
 }
